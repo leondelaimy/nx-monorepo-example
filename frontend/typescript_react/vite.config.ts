adb1952e397433bc9616a38b22783c0bcd5c5377
Leon Delaimy <leondelaimy@field.io>
2026-07-01 10:37:52 +0100

    fix: paths

--- frontend/typescript_react/vite.config.ts
+++ frontend/typescript_react/vite.config.ts
@@ -6,7 +6,7 @@ import { nxCopyAssetsPlugin } from '@nx/vite/plugins/nx-copy-assets.plugin';
 
 export default defineConfig(() => ({
   root: __dirname,
-  cacheDir: '../../node_modules/.vite/frontend/typescript-react',
+  cacheDir: '../../node_modules/.vite/frontend/typescript_react',
   server: {
     port: 4200,
     host: 'localhost',
@@ -21,7 +21,7 @@ export default defineConfig(() => ({
   //  plugins: [ nxViteTsPaths() ],
   // },
   build: {
-    outDir: '../../dist/frontend/typescript-react',
+    outDir: '../../dist/frontend/typescript_react',
     emptyOutDir: true,
     reportCompressedSize: true,
     commonjsOptions: {
@@ -35,7 +35,7 @@ export default defineConfig(() => ({
     include: ['{src,tests}/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
     reporters: ['default'],
     coverage: {
-      reportsDirectory: '../../coverage/frontend/typescript-react',
+      reportsDirectory: '../../coverage/frontend/typescript_react',
       provider: 'v8' as const,
     },
   },
